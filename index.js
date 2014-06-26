b0e6508f1da963dba635cc3e6657195acd71eb80
Cole R Lawrence <cole@zombiehippie.com>
2014-06-26 09:21:32 -0500

    Try hl post md

--- index.js
+++ index.js
@@ -1,78 +1,76 @@
 
 var highlight = require('pygmentize-bundled')
-var lift = require('lift-result/cps')
-var compile = lift(require('marked'))
-var ms = require('parse-duration')
-var fs = require('lift-result/fs')
-var rmdir = require('rm-r/sync')
-var get = require('solicit').get
-var map = require('map/async')
-var path = require('path')
-var ejs = require('ejs')
+var marked = require('marked')
 
-var template = fs.readFileSync(__dirname + '/index.html', 'utf8')
-var template = ejs.compile(template)
-var css = __dirname + '/css'
 
-if (fs.existsSync(css)) {
-  var stat = fs.statSync(css)
-  var age = Date.now() - stat.mtime
-} else {
-  fs.mkdirSync(css)
-  var age = Infinity
+function compile (contents, options, cCallback) {
+  var highlightRegex = /<pre><code class="lang-([^\s"]+)[\s\S]*?>([\s\S]+?)<\/code><\/pre>/g
+  function replaceCodeWithHighlights (html, omit, place, rCallback) {
+    if (2 === arguments.length) rCallback = omit
+    else {
+      html = html.replace(omit, place)
+    }
+    console.log (html)
+    var match = html.match(highlightRegex)
+    console.log (match)
+    if (match) {
+      highlight ({ lang: match[1], format: 'html' }, match[2], function (err, highlights) {
+        if (err) return rCallback(err)
+        replaceCodeWithHighlights(html, match[0], highlights, rCallback)
+      })
+    } else {
+      rCallback(null, html)
+    }
+  }
+  marked(contents, options, function (err, html) {
+    replaceCodeWithHighlights(html, cCallback)
+  })
 }
+var path = require('path')
+var fs = require('fs')
 
-// update css if old
-
-if (age > ms('5 days')) {
-  var urls = get('https://github.com/aheckmann/greadme').then(function(body){
-    var r = /href=["']([^"']+\.css)/g
-    var arr = []
-    var m
-    while (m = r.exec(body)) arr.push(m[1])
-    return arr
-  })
-  rmdir(css)
-  fs.mkdirSync(css)
-  var stylesheets = map(map(urls, get), function(src, i){
-    var name = css + '/' + i + '.css'
-    return fs.writeFile(name, src).yield(src)
-  })
-} else {
-  var stylesheets = map(fs.readdir(css), function(name){
-    return fs.readFile(css + '/' + name, 'utf8')
+var fileNameRegex = /([^\\\/]*)$/
+function bootstrapExtend (req, res, html) {
+  fs.readFile('vs.css', 'utf8', function (err, contents) {
+    res.end(
+      "<head>" +
+        "<title>" + req.url.match(fileNameRegex)[1] + "</title>" +
+        "<link rel='stylesheet' href='//maxcdn.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css'/>" +
+        "<style>" + (err ? "/*" + err + "*/" : contents) + "</style>" +
+      "</head>" +
+      "<body>" +
+        "<div class='container'>" +
+          html +
+        "</div>" +
+      "</body>"
+    )
   })
 }
 
 module.exports = function(opts){
   var dir = path.resolve(opts.directory)
+  var extend = opts.extend || bootstrapExtend
+  var regex = opts.regex || /\.m(?:d|arkdown)$/i 
 
   return function(req, res, next){
     var file = req.url
-
-    if (!(/\.m(?:d|arkdown)$/i).test(file)) return next()
+    if (!(regex).test(file)) return next()
 
     file = path.join(dir, file)
-
-    compile(fs.readFile(file, 'utf8'), {
-      gfm: true,
-      pedantic: false,
-      tables: true,
-      breaks: false,
-      sanitize: false,
-      smartLists: true,
-      smartypants: false,
-      highlight: function(code, lang, done){
-        highlight({lang: lang, format: 'html'}, code, done)
-      }
-    }).then(function(html){
-      return stylesheets.then(function(css){
-        res.end(template({
-          title: path.relative(dir, file),
-          markdown: html,
-          css: css
-        }))
+    fs.readFile(file, 'utf8', function (err, contents) {
+      if (err) return next(err)
+      compile(contents, {
+        gfm: true,
+        pedantic: false,
+        tables: true,
+        breaks: false,
+        sanitize: false,
+        smartLists: true,
+        smartypants: false
+      }, function (err, html) {
+        if (err) return next(err)
+        extend(req, res, html)
       })
-    }).read(null, next)
+    })
   }
 }
